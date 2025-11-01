c2a5dccedae81ff16b55fc60cfbcbc5f8de47d37
Noorhan Alayyoub <nalayyou@c1r3s11.irbid.42.school>
2025-11-01 13:02:10 +0300

    prolly wrong

---
+++ split.c
@@ -0,0 +1,84 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool	check(char c, char *charset)
+{
+	int	index;
+
+	index = 0;
+	while (charset[index])
+	{
+		if (charset[index] == c)
+			return (false);
+		index++;
+	}
+	return (true);
+}
+int	countstrings(char *str, char *charset)
+{
+	int		index;
+	int		count;
+	bool	flag;
+
+	flag = true;
+	index = 0;
+	count = 0;
+	while (str[index])
+	{
+		if (check(str[index], charset))
+		{
+			if (flag)
+				count++;
+			flag = false;
+		}
+		else
+			flag = true;
+		index++;
+	}
+}
+void	copystring(char *str, char **ptr, int index, int start, int end)
+{
+	int	j;
+
+	j = 0;
+	while (start < end)
+	{
+		ptr[index][j] = str[start];
+		start++;
+		j++;
+	}
+	ptr[index][end] = '\0';
+}
+
+void	smallallocate(char *str, char *charset, char **ptr)
+{
+	int	index;
+	int	start;
+	int	end;
+
+	start = 0;
+	end = 0;
+	index = 0;
+	while (index < numofstrings)
+	{
+		while (!check(str[start], charset))
+			start++;
+		while (checl(str[start], carset))
+			end++;
+		ptr[index] = malloc(end - start + 1);
+		copystring(str, ptr, index, start, end);
+		start = end;
+		index++;
+	}
+}
+char	**ft_split(char *str, char *charset)
+{
+	int numofstrings;
+	char **ptr;
+	numofstrings = countstrings(str, charset);
+	ptr = malloc((numofstrings + 1) * sizeof(char *));
+	ptr[numofstrings] = NULL;
+	smallallocate(str, charset, ptr);
+	return (ptr);
+}
